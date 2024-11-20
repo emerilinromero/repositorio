9d21e30281d55b9ddc68db66b05de3573be02564
emerilin <emeri1808@gmail.com>
2024-11-20 17:22:46 -0300

    nuevo

---
+++ app/src/main/java/com/emromero/testeteste2020/TaskCategory.kt
@@ -0,0 +1,7 @@
+package com.emromero.testeteste2020
+
+sealed class TaskCategory(var isSelected: Boolean = true) {
+    object Personal: TaskCategory()
+    object Business: TaskCategory()
+    object Other: TaskCategory()
+}
